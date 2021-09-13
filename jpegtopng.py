91692e6f934151a40e6a06b7e8e1a23fc9a69c42
HARSH SINGH <hgh9harsh@gmail.com>
2021-09-13 12:19:21 +0530

    DONE

---
+++ jpegtopng.py
@@ -0,0 +1,29 @@
+
+from PIL import Image, ImageFilter
+
+
+import sys,os
+
+current,fresh=sys.argv[1],sys.argv[2]
+
+#  for creating png files
+def makingPNG():
+     with Image.open(file) as imgFile:
+                imgFile.save(os.path.join(fresh,infile).replace(".jpg",".png")
+                ,"png")
+
+
+
+#  for looping through them and creating them
+for infile in os.listdir(current):
+    file=os.path.join(current,infile)
+    if os.path.isfile(file):
+        try:
+            makingPNG()
+        except:
+            os.mkdir(fresh)
+            makingPNG()
+           
+            
+            
+     
